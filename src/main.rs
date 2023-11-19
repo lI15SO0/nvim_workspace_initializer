90eefafd79425d5e198355f6cfcee8ddb4f4bffc
lI15SO0 <svvivi@outlook.com>
2023-11-20 02:08:40 +0800

    refactor: refactor all code. move assets file. feat(main.rs): handle error while create dirs and files.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::{command, Parser};
 
-#[derive(Debug, Parser)]
+#[derive(Parser)]
 #[command(author = "lI15SO0", version, about = "Init nvim workspace dir.", long_about = None)]
 struct Args {
     path: Option<String>,
@@ -9,102 +9,86 @@ struct Args {
     force: bool,
 }
 
-fn create_dirs(path: &str) {
-    let _ = std::fs::create_dir_all(path);
+fn create_dir(path: &str) {
+    std::fs::create_dir_all(path).is_err().then(||{
+        println!("Error: Failed to create dir: {}", path);
+        std::process::exit(1);
+    });
 }
 
-fn create_exrc(path: &str) {
-    let _ = std::fs::write(
-        format!("{}{}", path, ".nvim.lua"),
-        include_str!("./nvim.lua"),
-    );
-    println!("Write: {}{}", path, ".nvim.lua");
+fn create_dirs(root: &str, dirs: Vec<String>) {
+    dirs.iter()
+        .for_each(|dir| create_dir(&(format!("{}{}", root, dir))));
 }
 
-fn create_snip_package_file(path: &str) {
-    let _ = std::fs::write(
-        format!("{}{}", path, ".nvim/snip/package.json"),
-        include_str!("./package.json"),
-    );
-    println!("Write: {}{}", path, ".nvim/snip/package.json");
-
-    let _ = std::fs::write(
-        format!("{}{}", path, ".nvim/snip/global.json"),
-        include_str!("./global.json"),
-    );
-    println!("Write: {}{}", path, ".nvim/snip/global.json");
+fn write_file(path: &str, contents: &str) {
+    std::fs::write(path, contents).is_err().then(||{
+        println!("Error: Failed to write file: {}", path);
+        std::process::exit(1);
+    });
+    println!("Write: {}", path);
 }
 
-fn create_core_file(path: &str) {
-    let _ = std::fs::write(format!("{}{}", path, ".nvim/lua/ws/core/options.lua"), "");
-    println!("Write: {}{}", path, ".nvim/lua/ws/core/options.lua");
-    let _ = std::fs::write(format!("{}{}", path, ".nvim/lua/ws/core/keymaps.lua"), "");
-    println!("Write: {}{}", path, ".nvim/lua/ws/core/keymaps.lua");
-    let _ = std::fs::write(format!("{}{}", path, ".nvim/lua/ws/core/dap.lua"), include_str!("./dap.lua"));
-    println!("Write: {}{}", path, ".nvim/lua/ws/core/dap.lua");
+fn write_files(path: &str) {
+    [
+        (".nvim.lua", include_str!("./assets/nvim.lua")),
+        (".nvim/snip/package.json", include_str!("./assets/package.json"),),
+        (".nvim/snip/global.json", include_str!("./assets/global.json"),),
+        (".nvim/lua/ws/core/dap.lua", include_str!("./assets/dap.lua"),),
+        (".nvim/lua/ws/core/options.lua", ""),
+        (".nvim/lua/ws/core/keymaps.lua", ""),
+    ]
+    .iter()
+    .for_each(|(file, contents)| {
+        write_file(&format!("{}{}", path, file), contents);
+    });
 }
 
 fn already_is_ws(path: &str, force: bool) {
     if !force {
-        let exist = std::fs::metadata(path)
+        let exist = std::fs::metadata(format!("{}{}", path, ".nvim.lua"))
             .and_then(|_| Ok(true))
             .or_else(|_| Ok::<bool, String>(false))
             .unwrap();
 
         if exist {
             match path {
-                "./.nvim.lua" => {
-                    println!("This path already was nvim workspace.(You can run with \"-f\" to force init this path as workspace after you backup file.)");
+                "./" => {
+                    println!("This path already was nvim workspace.(run with \"-f\" to force init workspace.)");
                 }
                 _ => {
-                    println!("{} already exist.(You can run with \"-f\" to force init this path as workspace after you backup file.)", path);
+                    println!("{} already exist.(run with \"-f\" to force init workspace.)", path);
                 }
             }
             std::process::exit(0);
         }
     }
+
+    println!("Warning: Run at force mode.");
 }
 
 fn main() {
     let args = Args::parse();
 
-    let dirs: Vec<String> = include_str!("./dirs.txt")
+    let dirs: Vec<String> = include_str!("./assets/dirs.txt")
         .lines()
         .map(|f| f.to_string())
         .collect();
 
-    match args.path {
+    let path = match args.path {
         Some(path) => {
-            let p = path
-                .contains("\\")
-                .then(|| path.clone().replace("\\", "/"))
-                .or_else(|| (Some(path.clone())))
-                .unwrap();
-            let p = p
-                .ends_with("/")
+            let p = path.replace("\\", "/");
+            p.ends_with("/")
                 .then(|| p.clone())
                 .or_else(|| Some(format!("{}{}", p, '/')))
-                .unwrap();
-
-            already_is_ws(&path, args.force);
-
-            for i in dirs {
-                create_dirs(&(format!("{}{}", p, i)))
-            }
-            create_exrc(&p);
-            create_snip_package_file(&p);
-            create_core_file(&p);
-            println!("Generate done at \"{}\" .", path);
+                .unwrap()
         }
-        None => {
-            already_is_ws("./.nvim.lua", args.force);
-            for i in dirs {
-                create_dirs(&(format!("{}{}", "./", i)))
-            }
-            create_exrc("./");
-            create_snip_package_file("./");
-            create_core_file("./");
-            println!("Generate done at \"./\" .");
-        }
-    }
+        None => "./".to_string(),
+    };
+
+    already_is_ws(&path, args.force);
+    create_dirs(&path, dirs);
+    write_files(&path);
+
+    println!("Generate done at \"{}\" .", path);
 }
